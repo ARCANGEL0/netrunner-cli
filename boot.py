12aab5b44fcf21e5a0df4fccfb1a79a2f3060710
Henry <henry.arcangello@proton.me>
2025-04-29 20:23:24 +0000

    ad

--- boot.py
+++ boot.py
@@ -151,11 +151,12 @@ def run_breach(scr):
         sy, sx = max(2, (h - gh) // 2), max(2, (w - gw - 60) // 2)
         for r in range(GRID_SIZE * 2 + 2): scr.addstr(sy + r, sx, ' ' * (gw - 2))
         scr.clear()
-        typeT(scr, '\n\n\n\n\n\n//// NETRUNNER_V3.1 BREACH PROTOCOL RUNNING\n')
+        typeT(scr, '\n\n//// NETRUNNER_V3.1 BREACH PROTOCOL RUNNING\n')
         typeT(scr, '> ::: ENGAGING PROTOCOL//0x002D03F\n')
         typeT(scr, '> :: INITIATING_NET_OVERRIDE //\n')
         typeT(scr, '> ==>> NETVH_UPLOADED [███░░]\n')
         typeT(scr, '> >>> ROOT_NODE_ACCESS//GRANTED\n')
+        scr.clear()
     else:
         bloquearTela()
     scr.refresh()
